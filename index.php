4aa4eeae032b798a1f115eab3ee3b0c164d678d6
Nikita Kalitin <nikita.s.kalitin@gmail.com>
2019-10-29 12:22:16 +0300

    reorder <scrpt> in head for delete error message in console, check path to bootstrap.min.css

--- index.php
+++ index.php
@@ -8,12 +8,22 @@ require_once('vendor/autoload.php');
 
 use GUIHelper\UploadFile;
 
-$a = new UploadFile('./upload');
-echo "<pre>";
-print_r($_POST);
-print_r($_FILES);
-echo "</pre>"
-//$a->handlerQuery();
+session_start();
+
+if (isset($_SESSION['message']) && $_SESSION['message'])
+{
+    printf('<b>%s</b>', $_SESSION['message']);
+    unset($_SESSION['message']);
+}
+
+$fUp = new UploadFile('./upload');
+if (!empty($_POST)) {
+    header('Content-Type: application/json');
+    echo json_encode(["post: " => $_POST, "files: " => $_FILES], true);
+    $fUp->validateFile();
+    die;
+}
+
 ?>
 
 <!doctype html>
@@ -26,17 +36,17 @@ echo "</pre>"
 <!--    <form name="loadFile" action="" method="post" enctype="multipart/form-data">-->
 <form id="loadFile" name="loadFile" action="" method="post" enctype="multipart/form-data">
     <label>File to upload:</label>
-    <input id="inputFile" type="file" name="upfile" required />
-    <input id="submit" type="submit" value="Upload!" />
+    <input id="inputFile" type="file" name="upfile" required/>
+    <input id="submit" type="submit" value="Upload!"/>
 </form>
 <script>
   document.addEventListener('DOMContentLoaded', function () {
-    let sbmt = document.getElementById('submit1');
-    let form = document.getElementById('loadFile')
-    sbmt.addEventListener('click',function(e){
+    let sbmt = document.getElementById('submit');
+    let form = document.getElementById('loadFile');
+    sbmt.addEventListener('click', function (e) {
       e.preventDefault();
       var formData = new FormData(form);
-      formData.append("uploadFile", form.submit);
+      formData.append('uploadFile', form.submit);
       var xhr = new XMLHttpRequest();
       xhr.open('POST', '/index.php');
       xhr.send(formData);
--- src/UploadFile.php
+++ src/UploadFile.php
@@ -11,9 +11,10 @@ class UploadFile
 {
     private $path_to;
     private $path_out;
-//$target_dir = "uploads/";
-//$target_file = $target_dir . basename($_FILES["fileToUpload"]["name"]);
-//$uploadOk = 1;
+    private $target_file;
+    private $uploadOk;
+    private $fileType;
+
 
     public function sanitize($path_to, $path_out)
     {
@@ -26,6 +27,10 @@ class UploadFile
         $res = $this->sanitize($path_to, $path_out);
         $this->path_to = $res[0];
         $this->path_out = $res[1];
+        $this->target_file = $this->path_to.basename($_FILES["fileToUpload"]["name"]);
+        $this->uploadOk = 1;
+        $this->fileType = strtolower(pathinfo($this->target_file,PATHINFO_EXTENSION));
+
     }
 
     public function handlerQuery()
@@ -35,10 +40,11 @@ class UploadFile
 
     public function validateFile()
     {
+
         if (isset($_POST["submit"])) {
 
 // Check if file already exists
-            if (file_exists($target_file)) {
+            if (file_exists($this->target_file)) {
                 echo "Sorry, file already exists.";
                 $uploadOk = 0;
             }
@@ -48,9 +54,8 @@ class UploadFile
                 $uploadOk = 0;
             }
 // Allow certain file formats
-            if ($imageFileType != "jpg" && $imageFileType != "png" && $imageFileType != "jpeg"
-                && $imageFileType != "gif") {
-                echo "Sorry, only JPG, JPEG, PNG & GIF files are allowed.";
+            if ($this->target_file != "odt") {
+                echo "Sorry, only odt files are allowed.";
                 $uploadOk = 0;
             }
 // Check if $uploadOk is set to 0 by an error
@@ -58,7 +63,7 @@ class UploadFile
                 echo "Sorry, your file was not uploaded.";
 // if everything is ok, try to upload file
             } else {
-                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $target_file)) {
+                if (move_uploaded_file($_FILES["fileToUpload"]["tmp_name"], $this->target_file)) {
                     echo "The file " . basename($_FILES["fileToUpload"]["name"]) . " has been uploaded.";
                 } else {
                     echo "Sorry, there was an error uploading your file.";
